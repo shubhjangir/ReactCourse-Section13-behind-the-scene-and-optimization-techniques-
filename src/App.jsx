beff32bea6edb7e7c757cd8239cdfa6d4f620fc1
Shubham Jangir <shubhamjangir101@gmail.com>
2025-09-24 00:20:33 +0530

    when state is manage by keys

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       <main>
         <ConfigureCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
+        <Counter intialCount={0} />
       </main>
     </>
   );
